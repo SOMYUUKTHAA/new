67afadc7eaf16e8085d69559df97ad09be288149
SOMYUUKTHAA <30828746+SOMYUUKTHAA@users.noreply.github.com>
2018-03-12 16:28:51 +0530

    Update som79.cpp

--- som79.cpp
+++ som79.cpp
@@ -2,12 +2,17 @@
 using namespace std;
 int main()
 {
-    int n1,n2,res;
+    int n1,n2,res,i,j,a,flag=0;
     cout<<"enter two numbers:";
     cin>>n1>>n2;
     res=n1*n2;
-    n1*=n1;
-    if(res==n1)
+    for(i=1;i<res;i++)
+    {
+        a=i*i;
+        if(res==a)
+        flag=1;
+    }
+     if(flag==1)
     cout<<"Yes";
     else
     cout<<"No";
