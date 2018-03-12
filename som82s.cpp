ba13ac366bd5e2b365a3d85e71bb67ff86bc0a00
SOMYUUKTHAA <30828746+SOMYUUKTHAA@users.noreply.github.com>
2018-03-12 16:41:12 +0530

    Rename som82.cpp to som82s.cpp

---
+++ som82s.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float l,b,area;
+    cout<<"enter length & breadth of farm:";
+    cin>>l>>b;
+    area=l*b;
+    cout<<"Area of farm:"<<area;
+}
